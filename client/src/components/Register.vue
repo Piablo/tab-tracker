efba26a58c4fa837c177111fce61b5955ecfd8b2
unknown <paul.buys1@gmail.com>
2018-09-12 13:08:47 +0200

    vuetify

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,23 +1,35 @@
 <template>
-  <div>
-    <h1>Register</h1>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
 
-
-    <input 
-      type="email"
-      name="email"
-      v-model="email"
-      placeholder="email"/>
-    <br>
-    <input 
-      type="password"
-      name="password"
-      v-model="password"
-      placeholder="password"/>
-    <br>
-    <button
-      @click="register">register</button>
-  </div>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input 
+            type="email"
+            name="email"
+            v-model="email"
+            placeholder="email"/>
+          <br>
+          <input 
+            type="password"
+            name="password"
+            v-model="password"
+            placeholder="password"/>
+          <br>
+          <div class="error" v-html="error"/> 
+          <br>
+          <v-btn
+          class="cyan"
+            @click="register">
+            Register
+          </v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -25,22 +37,21 @@ import AuthenticationService from '@/services/AuthenticationService'
 export default {
   data() {
     return {
-      email: 'anb',
-      password: '123'
+      email: '',
+      password: '',
+      error: null
     };
-  },
-  watch:{
-    email (value){
-      console.log('email has changed value')
-    }
   },
   methods: {
     async register() {
-      const response = await AuthenticationService.register({
-        email:this.email,
-        password: this.password
-      })
-      console.log(response.data);
+      try{
+        await AuthenticationService.register({
+          email:this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   },
   mounted (){
@@ -51,6 +62,9 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.error {
+  color:red;
+}
 
 </style>
--- server/src/app.js
+++ server/src/app.js
@@ -2,16 +2,48 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const {sequelize} = require('./models')
+const config = require('./config/config')
+//const mysql = require('mysql');
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/register', (req, res) =>{
-    res.send({
-        message: `Hello ${req.body.email} your user was registered`
-    })
-})
+require('./routes')(app)
 
-app.listen(process.env.PORT || 8081);
+// console.log("does this run");
+
+// var connection = mysql.createConnection({
+//     host:'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'articles'
+// });
+
+// connection.connect();
+
+// var article = {
+//     author: 'Paul Buys',
+//     title: 'Node tut',
+//     body: 'foo bar'
+// }
+
+// var query = connection.query('SELECT * FROM articles', article, function (err, result) {
+//     if(err) {
+//         console.error(err);
+//         return;
+//     }
+//     console.log("where does this get logged");
+//     console.error(result);
+// })
+
+// console.log("and this guy?");
+
+sequelize.sync()
+    .then(() => {
+        app.listen(config.port);
+        console.log(`Server started on port ${config.port}`)
+    })
+
